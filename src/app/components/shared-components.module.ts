0a79353e8192681f2a26648850b856d51fbddb9e
Ariel Mendez <arielmendez735@gmail.com>
2019-11-19 01:22:10 -0800

    Add last changes

--- src/app/components/shared-components.module.ts
+++ src/app/components/shared-components.module.ts
@@ -1,20 +1,13 @@
 import { ClassesComponent } from './classes/classes.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
 import { IonicModule } from '@ionic/angular';
-=======
->>>>>>> 80b4dc5523f943c43fe03c9e842b7f67844f8b8b
 
 @NgModule({
   declarations: [ClassesComponent],
   imports: [
-<<<<<<< HEAD
     CommonModule,
     IonicModule
-=======
-    CommonModule
->>>>>>> 80b4dc5523f943c43fe03c9e842b7f67844f8b8b
   ],
   exports: [ClassesComponent]
 })
